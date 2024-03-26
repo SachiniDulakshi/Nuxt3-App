17747af38c201bc63569aaed78b74d0c62daa800
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-26 15:50:03 +0530

    Add Reusable Components

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,7 +2,7 @@
     <div>
       <div class="grid grid-cols-4 gap-5">
         <div v-for="(p, index) in products" :key="index">
-          <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+          <ProductCard :product="p" />
         </div>
       </div>
     </div>
