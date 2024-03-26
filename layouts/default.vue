62fbbc5019427bdee3a02c22f745ced6fedbe1ef
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-26 15:11:12 +0530

    Add tailwind css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,17 @@
+
 <template>
     <div>
-      <header>
-        <nav>
-          <NuxtLink to="/">Nuxt3APP</NuxtLink>
-          <ul >
+      <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+          <NuxtLink to="/" class="font-bold">Nuxt3APP</NuxtLink>
+          <ul class="flex gap-4">
             <li><NuxtLink to="/">Home</NuxtLink></li>
             <li><NuxtLink to="/about">About</NuxtLink></li>
-            <li><NuxtLink to="/products">Merch</NuxtLink></li>
+            <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
           </ul>
         </nav>
       </header>
-      <div>
+      <div class="container mx-auto p-4">
         <slot />
       </div>
     </div>
