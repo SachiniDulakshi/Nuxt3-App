6b529eb0952e6b776e08ca3244ca7ad28f71631f
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-26 12:00:39 +0530

    Add route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div>
+        <h1>Product details</h1>
+        <p>Product details for {{id}} </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit et non expedita consequatur ipsa deserunt maxime sed quibusdam at necessitatibus, asperiores temporibus adipisci delectus praesentium aliquid facere vero, consequuntur voluptas?</p>
+    </div>
+</template>
+
+<script setup>
+    const {id} = useRoute().params
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
