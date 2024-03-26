e216058ba9ebfff23810d08b7a26735372a2f23d
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-26 21:52:03 +0530

    Add server routes and fetch data from currency api

--- pages/about.vue
+++ pages/about.vue
@@ -1,24 +1,25 @@
 <template>
     <div>
-
-        <h1>About Us</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et culpa non ipsa voluptatum necessitatibus eos consectetur modi praesentium repellendus ratione voluptate, nesciunt tempora. Molestiae quasi ab illo hic sunt eius.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam odit sunt, facere, voluptatem doloremque odio iste laboriosam quis cum recusandae similique nam voluptas perspiciatis quaerat sint reprehenderit corporis provident consectetur!</p>
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet aperiam minima unde nemo harum quam maxime optio quos corrupti. Eum sapiente facere nemo, laborum ullam non cum fuga quas eveniet harum molestiae minus atque vel ratione illo quia, iure commodi dicta porro excepturi quam. Facere, commodi corrupti ipsam neque totam reprehenderit laboriosam laborum veniam est quo, repellat saepe impedit labore?</p>
+      <div>{{ data }}</div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style scoped>
-h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-}
-
-p {
-    margin: 20px 0;
-}
-
-</style>
+  </template>
+  
+  <style scoped>
+    h2 {
+      margin-bottom: 20px;
+      font-size: 36px;
+    }
+    p {
+      margin: 20px 0;
+    }
+  </style>
+  
+  <script setup>
+    // const { data } = await useFetch('/api/custom?name=mario', {
+    //   method: 'post',
+    //   body: {age: 30}
+    // })
+    const { data } = await useFetch('/api/custom')
+  </script>
