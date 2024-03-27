ee2c5d4d645a6374f28f95c8849c1108fa01a39f
Malshi623 <mhsm.dulakshi@gmail.com>
2024-03-27 15:11:19 +0530

    Add cart

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <header class="shadow-sm bg-white">
-      <nav class="container mx-auto p-4">
+      <nav class="container mx-auto p-4 flex justify-between">
         <NuxtLink to="/products" class="font-bold">Nuxt3APP Merch</NuxtLink>
+        <ul class="flex gap-4">
+          <li><nuxt-link to="/products/cart.vue"><Icon name="icons8:add-shopping-cart" /></nuxt-link></li>   
+        </ul>
       </nav>
     </header>
     <div class="container mx-auto p-4">
